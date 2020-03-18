51b6c669da0181863a2aec3aa7819b13c7def5db
StlMaris123 <stellamarisnjage@gmail.com>
2020-03-19 01:06:26 +0200

    making a request from an action creator

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PostList from './PostList';
-
 const App = () => {
     return(
         <div className="ui container"><PostList /></div>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { fetchPosts } from '../actions';
+
 
 class PostList extends React.Component {
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
     render(){
     return (
         <div> I am a Post</div>
     )
 }
 }
-export default PostList;
+export default connect(null, {fetchPosts})(PostList);
